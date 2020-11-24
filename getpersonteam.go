75fd3a4251a278dd6060bdb187ab086f0b26aee6
bmheenan <bmheenan@users.noreply.github.com>
2020-11-24 08:02:41 -0500

    - Personteam -> Stakeholder

--- getpersonteam.go
+++ getpersonteam.go
@@ -1,22 +1,26 @@
 package tapfed
 
-import "time"
+import (
+	"time"
+
+	"github.com/bmheenan/taps"
+)
 
 // GetPersonteam takes a person or team's email and returns all the info for them.
-func GetPersonteam(email string, cb func(Personteam, error)) {
+func GetPersonteam(email string, cb func(taps.Stakeholder, error)) {
 	time.Sleep(300 * time.Millisecond) // simulate network traffic
 	if email == "brandon@ex.com" {
-		cb(Personteam{
+		cb(taps.Stakeholder{
 			Email: "brandon@ex.com",
 			Name:  "Brandon",
 		}, nil)
 	} else if email == "steph@ex.com" {
-		cb(Personteam{
+		cb(taps.Stakeholder{
 			Email: "steph@ex.com",
 			Name:  "Steph",
 		}, nil)
 	} else if email == "frontend@ex.com" {
-		cb(Personteam{
+		cb(taps.Stakeholder{
 			Email: "frontend@ex.com",
 			Name:  "Frontend",
 		}, nil)

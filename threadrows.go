16cf4d5ae6bc0698ac803aad818d52a09b83d842
bmheenan <bmheenan@users.noreply.github.com>
2020-11-27 14:16:36 -0500

    + GetThreadrowsForParentIter + GetThreadrowsForChild

--- threadrows.go
+++ threadrows.go
@@ -19,3 +19,31 @@ func GetThreadrowsForStkIter(stk, iter string, chThs chan<- []taps.Threadrow, ch
 		chThs <- b.Ths
 	}()
 }
+
+// GetThreadrowsForParentIter returns all threads (as hierarchical threadrows) for the given parent thread `parent`
+// and iteration `iter`. It returns immediately, then sends results via the channel `chThs`, or an error via `chErr`
+func GetThreadrowsForParentIter(parent int64, iter string, chThs chan<- []taps.Threadrow, chErr chan<- error) {
+	go func() {
+		b := taps.APIThreadrowsGetRes{}
+		err := decodeGet(fmt.Sprintf("%s/threadrows/?parent=%d&iter=%s", baseURL, parent, iter), &b)
+		if err != nil {
+			chErr <- err
+			return
+		}
+		chThs <- b.Ths
+	}()
+}
+
+// GetThreadrowsForChild returns all threads (as flat threadrows) for the given child thread `child`. It
+// returns immediately, then sends results via the channel `chThs`, or an error via `chErr`
+func GetThreadrowsForChild(child int64, chThs chan<- []taps.Threadrow, chErr chan<- error) {
+	go func() {
+		b := taps.APIThreadrowsGetRes{}
+		err := decodeGet(fmt.Sprintf("%s/threadrows/?child=%d", baseURL, child), &b)
+		if err != nil {
+			chErr <- err
+			return
+		}
+		chThs <- b.Ths
+	}()
+}

5894cf974e12bf9190c65d5b7aaeb6767cf1a1d6
bmheenan <bmheenan@users.noreply.github.com>
2020-11-28 14:28:45 -0500

    + GetItersForParent

--- iterations.go
+++ iterations.go
@@ -19,3 +19,17 @@ func GetItersForStk(stk string, chIters chan<- []string, chErr chan<- error) {
 		chIters <- b.Iters
 	}()
 }
+
+// GetItersForParent returns all iterations for the given parent `pa` via the channel `chIters`, or an error via the
+// channel `chErr`
+func GetItersForParent(pa int64, chIters chan<- []string, chErr chan<- error) {
+	go func() {
+		b := taps.APIItersGetRes{}
+		err := decodeGet(fmt.Sprintf("%s/iterations/?parent=%d", baseURL, pa), &b)
+		if err != nil {
+			chErr <- err
+			return
+		}
+		chIters <- b.Iters
+	}()
+}

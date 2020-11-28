b33b4bb854cf2710be1558cc3edee225547f6828
bmheenan <bmheenan@users.noreply.github.com>
2020-11-27 19:27:05 -0500

    + GetThread

--- go.mod
+++ go.mod
@@ -3,6 +3,6 @@ module github.com/bmheenan/tapfed
 go 1.15
 
 require (
-	github.com/bmheenan/taps v0.0.0-20201126194231-0a95db7341ec
+	github.com/bmheenan/taps v0.0.0-20201127214932-d81a4ee5fe63
 	github.com/vugu/vugu v0.3.3
 )
--- threads.go
+++ threads.go
@@ -9,6 +9,25 @@ import (
 	"github.com/bmheenan/taps"
 )
 
+// GetThread returns the thread corresponding to the given `id`. It returns immediately, then sends the thread via
+// the chTh channel, or an error via chErr
+func GetThread(id int64, chTh chan<- taps.Thread, chErr chan<- error) {
+	go func() {
+		res, err := http.Get(fmt.Sprintf("%s/threads/%d", baseURL, id))
+		if err != nil {
+			chErr <- fmt.Errorf("Could not call API: %v", err)
+			return
+		}
+		b := taps.APIThreadsGetRes{}
+		err = json.NewDecoder(res.Body).Decode(&b)
+		if err != nil {
+			chErr <- fmt.Errorf("Could not decode response: %v", err)
+			return
+		}
+		chTh <- b.Th
+	}()
+}
+
 // NewThread creates a new thread with the given info. It returns immediately, then returns the thread's ID via
 // `chID` or an error via `chErr`
 func NewThread(name, owner, iter string, cost int, pas, chs []int64, chID chan<- int64, chErr chan<- error) {
